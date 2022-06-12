9364e7b2e40cf0b7e1287932e53bbe51f4f9b2a4
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-06-13 00:21:43 +0530

    Make site responsive

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+// Alerts user once
+var alerted = localStorage.getItem("alerted") || "";
+if (alerted != "yes") {
+  alert("Refesh page to Retry");
+  localStorage.setItem("alerted", "yes");
+}
+// Generate Random Numbers
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+// Update the Dice
 var img1 = `images/dice${randomNumber1}.png`;
 document.querySelector(".img1").setAttribute("src", img1);
 
 var img2 = `images/dice${randomNumber2}.png`;
 document.querySelector(".img2").setAttribute("src", img2);
 
+// Check the winner
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML =
-    '<img style="width: 7%; margin: auto 30px;" src="images/flag.png"></img>Player 1 Wins!';
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 } else if (randomNumber1 == randomNumber2) {
   document.querySelector("h1").innerHTML = "Draw!";
 } else {
-  document.querySelector("h1").innerHTML =
-    'Player 2 Wins!<img style="width: 7%; margin: auto 30px;" src="images/flag.png"></img>';
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
